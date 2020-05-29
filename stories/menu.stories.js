0e6480e6d77c11100984d3e65d61b0089acce546
Christian Boyle <mail@christianboyle.com>
2020-05-29 13:10:41 -0500

    Add Menu stories (Prefix/Suffix Icon, w/Divider, w/Header)

--- stories/menu.stories.js
+++ stories/menu.stories.js
@@ -11,12 +11,12 @@ export default {
   },
 };
 
-const menu_basic_html = `<div class="dropdown-menu show" style="position: relative; top: 0;">
+const menu_basic_html = `<div class="dropdown-menu show" style="position: relative; top: 0; margin: 0 20px 20px 0;">
   <a class="dropdown-item" href="#">Action</a>
   <a class="dropdown-item" href="#">Another action</a>
   <a class="dropdown-item" href="#">Something else here</a>
 </div>
-<div class="dropdown-menu show" style="position: relative; top: 0; left: 20px; max-width: 400px;">
+<div class="dropdown-menu show" style="position: relative; top: 0; max-width: 400px;">
   <a class="dropdown-item text-truncate" href="#">Seven Steps For Keeping Teeth Healthy For Animals</a>
   <a class="dropdown-item" href="#">Forward</a>
   <a class="dropdown-item" href="#">Process</a>
@@ -42,3 +42,111 @@ MenuBasic.story = {
 
 
 
+const menu_prefix_icon_html = `<div class="dropdown-menu show" style="position: relative; top: 0; margin: 0 20px 20px 0;">
+  <a class="dropdown-item" href="#"><i class="icn-search mr-2 mt-1 float-left"></i>Chad Hampton</a>
+  <a class="dropdown-item" href="#"><i class="icn-search mr-2 mt-1 float-left"></i>Chad Hampton</a>
+  <a class="dropdown-item" href="#"><i class="icn-search mr-2 mt-1 float-left"></i>Chad Hampton</a>
+</div>`;
+
+export const MenuPrefixIcon = () => `<div class="container-fluid">${menu_prefix_icon_html}</div>`;
+
+MenuPrefixIcon.story = { 
+    name: 'Menu Prefix Icon',
+    parameters: { 
+      notes: `
+      
+      #Sample Code
+
+      \`\`\`html
+      ${menu_prefix_icon_html}
+      \`\`\`
+
+      `
+    }
+  
+};
+
+
+
+const menu_suffix_icon_html = `<div class="dropdown-menu show" style="position: relative; top: 0; margin: 0 20px 20px 0;">
+  <a class="dropdown-item" href="#">Chad Hampton<i class="icn-chevron_right ml-3 mt-1 float-right"></i></a>
+  <a class="dropdown-item" href="#">Chad Hampton<i class="icn-chevron_right ml-3 mt-1 float-right"></i></a>
+  <a class="dropdown-item" href="#">Chad Hampton<i class="icn-chevron_right ml-3 mt-1 float-right"></i></a>
+</div>`;
+
+export const MenuSuffixIcon = () => `<div class="container-fluid">${menu_suffix_icon_html}</div>`;
+
+MenuSuffixIcon.story = { 
+    name: 'Menu Suffix Icon',
+    parameters: { 
+      notes: `
+      
+      #Sample Code
+
+      \`\`\`html
+      ${menu_suffix_icon_html}
+      \`\`\`
+
+      `
+    }
+  
+};
+
+
+const menu_divider_html = `<div class="dropdown-menu show" style="position: relative; top: 0; margin: 0 20px 20px 0;">
+  <a class="dropdown-item" href="#">Add to Career Portfolio</a>
+  <a class="dropdown-item" href="#">Remove QR Code</a>
+  <a class="dropdown-item" href="#">View as Word</a>
+  <div class="dropdown-divider"></div>
+  <a class="dropdown-item btn-danger" href="#">Delete</a>
+</div>`;
+
+export const MenuDivider = () => `<div class="container-fluid">${menu_divider_html}</div>`;
+
+MenuDivider.story = { 
+    name: 'Menu with Divider',
+    parameters: { 
+      notes: `
+      
+      #Sample Code
+
+      \`\`\`html
+      ${menu_divider_html}
+      \`\`\`
+
+      `
+    }
+  
+};
+
+
+
+const menu_header_html = `<div class="dropdown-menu show" style="position: relative; top: 0; margin: 0 20px 20px 0;">
+  <h6 class="dropdown-header">16/24 bold</h6>
+  <a class="dropdown-item" href="#">Add to Career Portfolio</a>
+  <a class="dropdown-item" href="#">Remove QR Code</a>
+  <a class="dropdown-item" href="#">View as Word</a>
+  <div class="dropdown-divider"></div>
+  <h6 class="dropdown-header">Tools</h6>
+  <a class="dropdown-item" href="#">Add to Career Portfolio</a>
+  <a class="dropdown-item" href="#">Remove QR Code</a>
+  <a class="dropdown-item" href="#">View as Word</a>
+</div>`;
+
+export const MenuHeader = () => `<div class="container-fluid">${menu_header_html}</div>`;
+
+MenuHeader.story = { 
+    name: 'Menu with Header',
+    parameters: { 
+      notes: `
+      
+      #Sample Code
+
+      \`\`\`html
+      ${menu_header_html}
+      \`\`\`
+
+      `
+    }
+  
+};
