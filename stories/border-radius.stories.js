e8b430059e5170896d196a2d4aefce40f5961ce5
Christian Boyle <mail@christianboyle.com>
2020-06-17 11:49:00 -0500

    Add .rounded-xl example to Border Radius story

--- stories/border-radius.stories.js
+++ stories/border-radius.stories.js
@@ -38,6 +38,15 @@ const borderRadiusHTML = `
       <text x="35%" y="65%" fill="#dee2e6" dy=".3em">2px</text>
     </svg>
   </div>
+  <br>
+  <div>
+    <svg class="bd-placeholder-img rounded-xl" width="75" height="75" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Example small rounded image: 75x75">
+      <title>Example extra large rounded image</title>
+      <rect width="100%" height="100%" fill="#868e96"></rect>
+      <text x="15%" y="40%" fill="#dee2e6" dy=".3em">XLarge</text>
+      <text x="25%" y="65%" fill="#dee2e6" dy=".3em">10px</text>
+    </svg>
+  </div>
 </div>`; 
 export const BorderRadius = () => borderRadiusHTML;
 
