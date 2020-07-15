bd745e2c9a7030a46561cfe2ad41682f69623797
Christian Boyle <mail@christianboyle.com>
2020-07-15 12:34:56 -0500

    Add aria-label to More (3-dot) buttons to fix a11y error

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -205,7 +205,7 @@ const groupHTML = () => withActions('click', 'mouseover')(() => `
 <div class="btn-group" role="group" aria-label="Basic example">
   <button type="button" class="btn btn-outline-primary">Action1</button>
   <button type="button" class="btn btn-outline-primary">Action2</button>
-  <button type="button" class="btn btn-outline-primary"><i class="icn icn-more_horizontal align-middle h1"></i></button>
+  <button type="button" class="btn btn-outline-primary" aria-label="More Actions"><i class="icn icn-more_horizontal align-middle h1"></i></button>
 </div>
 <br><br>
 <h3>Usage</h3>
@@ -214,7 +214,7 @@ const groupHTML = () => withActions('click', 'mouseover')(() => `
   <div class="btn-group" role="group" aria-label="Usage example">
     <button type="button" class="btn btn-outline-primary">Edit</button>
     <button type="button" class="btn btn-outline-primary">Forward</button>
-    <button type="button" class="btn btn-outline-primary"><i class="icn icn-more_horizontal align-middle h1"></i></button>
+    <button type="button" class="btn btn-outline-primary" aria-label="More Actions"><i class="icn icn-more_horizontal align-middle h1"></i></button>
   </div>
 </div>
 <br>
