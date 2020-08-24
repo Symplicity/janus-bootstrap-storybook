0efbc073ad1214ff4ade96d59611f6fbea17ad31
Christian Boyle <mail@christianboyle.com>
2020-08-24 14:50:56 -0500

    Add Form/Input [type=email] story (missed file in last commit) and update @Symplicity/bootstrap

---
+++ stories/form.stories.js
@@ -0,0 +1,50 @@
+import { document, setTimeout } from 'global';
+
+import {
+  array,
+  boolean,
+  button,
+  color,
+  date,
+  select,
+  withKnobs,
+  text,
+  number,
+} from '@storybook/addon-knobs';
+
+
+export default {
+  title: 'Components/Form/Input',
+  decorators: [withKnobs],
+  parameters: {
+    options: {
+      selectedPanel: 'storybook/a11y/panel',
+    },
+  },
+};
+
+const input_email_html = `
+<form>
+  <div class="mb-3">
+    <label for="email" class="form-label mb-0 h4">Email</label>
+    <div id="emailHelpBlock" class="form-text">Please enter an email (not school)</div>
+    <input type="email" class="form-control" id="email" placeholder="name@example.com" aria-describedby="emailHelpBlock">
+  </div>
+</form>
+`;
+
+export const InputEmail = () => `<div class="container-fluid">${input_email_html}</div>`;
+
+InputEmail.storyName = '[type="email"]';
+
+InputEmail.parameters = { 
+  notes: `
+  
+  #Sample Code
+
+  \`\`\`html
+  ${input_email_html}
+  \`\`\`
+
+  `
+};
