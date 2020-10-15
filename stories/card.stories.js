f6030b0b6bf127f6a7ffd87eaff0c767afc3268e
Christian Boyle <mail@christianboyle.com>
2020-10-15 08:20:58 -0500

    Add knobs for icon/icon-bg/title to card story

--- stories/card.stories.js
+++ stories/card.stories.js
@@ -1,7 +1,20 @@
 import { document, setTimeout } from 'global';
 
+import {
+  array,
+  boolean,
+  button,
+  color,
+  date,
+  select,
+  withKnobs,
+  text,
+  number,
+} from '@storybook/addon-knobs';
+
 export default {
   title: 'Components/Cards',
+  decorators: [withKnobs],
   parameters: {
     options: {
       selectedPanel: 'storybook/a11y/panel',
@@ -21,18 +34,51 @@ const card_basic_html = `
   </div>
 </div>`;
 
-export const CardBasic = () => `<div class="container-fluid">${card_basic_html}</div>`;
+export const CardBasic = () => {
+
+//`<div class="container-fluid">${card_basic_html}</div>`;
+
+  const icon_bg_color_options = {
+    "Pale Green": "bg-pale-green",
+    "Pale Orange": "bg-pale-orange",
+    "Pale Indigo": "bg-pale-indigo",
+    "Pale Purple": "bg-pale-purple",
+    "Pale Blue": "bg-pale-blue",
+    "Pale Red": "bg-pale-red"      
+  }
+
+  const card_title_text = text("Title", "Events");
+  const card_icon_class = text("Unicon Class", "icn-clock_filled");
+  const card_icon_bg_color = select(
+      "Icon Background Color",
+      icon_bg_color_options,
+      "bg-pale-green"
+  );
+
+  return `
+  <div style="width: 18rem;">
+    <div class="card">
+      <h3 class="card-header text-uppercase"><span class="${card_icon_class} ${card_icon_bg_color}"></span>${card_title_text}</h3>
+      <div class="card-body">
+        <h3 class="card-title">Special title treatment</h3>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  `
+};
 
 CardBasic.storyName = 'Card Basic';
-
+  
 CardBasic.parameters = { 
   notes: `
   
-  #Sample Code
+  #Sample Code 
 
   \`\`\`html
   ${card_basic_html}
   \`\`\`
-
+  
   `
 }
