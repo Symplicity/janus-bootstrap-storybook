ba840dc7bad73a882f5e155cc21ca75b2c6860bf
Christian Boyle <mail@christianboyle.com>
2020-11-06 10:21:29 -0600

    Add links story (default, primary, secondary, colored)

---
+++ stories/link.notes.md
@@ -0,0 +1,10 @@
+## Usage
+
+###Good
+
+- When the action results in the direct creation, editing, or deleting of data
+- When the action kicks off a workflow whose purpose is provide creation, editing, or deleting data. 
+
+###Bad
+
+- When the action navigates the user to a new page. Use a link.
---
+++ stories/link.stories.js
@@ -0,0 +1,181 @@
+import linkNotes from './link.notes.md';
+
+export default {
+  title: 'Components/Links',
+  parameters: {
+    options: {
+      selectedPanel: 'storybook/a11y/panel'
+    }
+  }
+};
+
+const defaultHTML = ` 
+<h3>Default</h3>
+<a href="#">Text link 1</a>
+
+<p>Adwords Keyword research for beginners
+When you embard on your first PPC
+journey, you need to keep a small number
+of keywords at first. Keyword lists are
+thousands of <a href="#">Text link 1</a> 
+should be left to the more experienced PPC marketer.
+Ideally, a beginner should use around 100</p>
+
+`;
+
+export const Default = () => defaultHTML;
+
+Default.storyName = 'Default';
+
+Default.parameters = {
+  notes: `
+    # Default Links
+
+    ## Documentation
+    
+    Here are some notes on default links. 
+    
+    ## Code Sample
+    
+    \`\`\`html
+    ${defaultHTML}
+    \`\`\`
+    
+    ${linkNotes}
+    
+    `
+};
+
+
+const primaryHTML = ` 
+<h3>Primary</h3>
+<a href="#" class="link-primary">Text link 1</a>
+
+<p>Adwords Keyword research for beginners
+When you embard on your first PPC
+journey, you need to keep a small number
+of keywords at first. Keyword lists are
+thousands of <a href="#" class="link-primary">Text link 1</a> 
+should be left to the more experienced PPC marketer.
+Ideally, a beginner should use around 100</p>
+
+`;
+
+export const Primary = () => primaryHTML;
+
+Primary.storyName = 'Primary';
+
+Primary.parameters = {
+  notes: `
+    # Primary Links
+
+    ## Documentation
+    
+    Here are some notes on primary links. 
+    
+    ## Code Sample
+    
+    \`\`\`html
+    ${primaryHTML}
+    \`\`\`
+    
+    ${linkNotes}
+    
+    `
+};
+
+
+const secondaryHTML = ` 
+<h3>Secondary</h3>
+<a href="#" class="link-secondary">Text link 1</a>
+
+<p>Adwords Keyword research for beginners
+When you embard on your first PPC
+journey, you need to keep a small number
+of keywords at first. Keyword lists are
+thousands of <a href="#" class="link-secondary">Text link 1</a> 
+should be left to the more experienced PPC marketer.
+Ideally, a beginner should use around 100</p>
+
+`;
+
+export const Secondary = () => secondaryHTML;
+
+Secondary.storyName = 'Secondary';
+
+Secondary.parameters = {
+  notes: `
+    # Secondary Links
+
+    ## Documentation
+    
+    Here are some notes on secondary links. 
+    
+    ## Code Sample
+    
+    \`\`\`html
+    ${secondaryHTML}
+    \`\`\`
+    
+    ${linkNotes}
+    
+    `
+};
+
+
+const coloredHTML = ` 
+<h3>Colored</h3>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<a href="#" class="link-primary">Primary link</a>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<a href="#" class="link-secondary">Secondary link</a>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<a href="#" class="link-success">Success link</a>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<a href="#" class="link-danger">Danger link</a>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<a href="#" class="link-warning">Warning link</a>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<a href="#" class="link-info">Info link</a>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<a href="#" class="link-light">Light link</a>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<a href="#" class="link-dark">Dark link</a>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<a href="#" class="link-pale-green">Pale-Green link</a>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<a href="#" class="link-pale-orange">Pale-Orange link</a>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<a href="#" class="link-pale-indigo">Pale-Indigo link</a>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<a href="#" class="link-pale-purple">Pale-Purple link</a>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<a href="#" class="link-pale-blue">Pale-Blue link</a>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+<a href="#" class="link-pale-red">Pale-Red link</a>
+
+`;
+
+export const Colored = () => coloredHTML;
+
+Colored.storyName = 'Colored';
+
+Colored.parameters = {
+  notes: `
+    # Colored Links
+
+    ## Documentation
+    
+    Here are some notes on Colored links. 
+    
+    ## Code Sample
+    
+    \`\`\`html
+    ${coloredHTML}
+    \`\`\`
+    
+    ${linkNotes}
+    
+    `
+};
