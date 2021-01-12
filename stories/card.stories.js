a85a1fedfe18c615f0e8fb81c1d45bf7cea9f3ae
Christian Boyle <mail@christianboyle.com>
2021-01-12 10:19:59 -0600

    Add Backstop reference image forgetten in previous commit (Basic->Homepage)

--- stories/card.stories.js
+++ stories/card.stories.js
@@ -319,3 +319,41 @@ CardSidebar.parameters = {
   `
 };
 
+const card_group = `
+    <div class="card">
+      <svg class="bd-placeholder-img card-img-top" width="100%" height="150" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+      <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>  
+`;
+
+export const CardGroup = () => `<div style="width: 18em;">${card_group}</div>`;
+
+CardGroup.storyName = 'Group';
+
+CardGroup.parameters = { 
+  notes: `
+
+  #Notes
+  Some info about cards: https://www.nngroup.com/articles/cards-component/
+  
+  #Sample Code
+
+  \`\`\`html
+  ${card_sidebar}
+  \`\`\`
+
+  `
+};
+
