5ff6c314b31d02073090bf209ec5602a1e2236c4
Christian Boyle <mail@christianboyle.com>
2021-01-13 09:24:41 -0600

    Add Cards > Group story

--- stories/card.stories.js
+++ stories/card.stories.js
@@ -320,7 +320,7 @@ CardSidebar.parameters = {
 };
 
 const card_group = `
-    <div class="card">
+    <div class="card card-alt">
       <svg class="bd-placeholder-img card-img-top" width="100%" height="150" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
