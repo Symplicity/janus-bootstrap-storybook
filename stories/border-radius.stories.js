952384d1aa467b022ef1d9a51950f27dc1dadbdb
Christian Boyle <mail@christianboyle.com>
2021-01-17 08:35:06 -0600

    Update border-radius story with backstop regression fixes and upstream twbs updates (custom name -> utilities api)

--- stories/border-radius.stories.js
+++ stories/border-radius.stories.js
@@ -10,38 +10,56 @@ export default {
 
 const borderRadiusHTML = `
   <div>
-    <svg class="bd-placeholder-img rounded" width="75" height="75" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Example small rounded image: 75x75">
-      <title>Example small rounded image</title>
+    <svg class="bd-placeholder-img rounded-0" width="75" height="75" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Example small rounded image: 75x75">
+      <title>Example zero rounded image</title>
       <rect width="100%" height="100%" fill="#868e96"></rect>
-      <text x="16%" y="40%" fill="#dee2e6" dy=".3em">Default</text>
-      <text x="33%" y="65%" fill="#dee2e6" dy=".3em">4px</text>
+      <text x="50%" y="40%" fill="#dee2e6" dy=".3em">Zero</text>
+      <text x="50%" y="65%" fill="#dee2e6" dy=".3em">0px</text>
     </svg>
   </div>
   <br>
   <div>
-    <svg class="bd-placeholder-img rounded-sm" width="75" height="75" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Example small rounded image: 75x75">
+    <svg class="bd-placeholder-img rounded-1" width="75" height="75" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Example small rounded image: 75x75">
       <title>Example small rounded image</title>
       <rect width="100%" height="100%" fill="#868e96"></rect>
-      <text x="25%" y="40%" fill="#dee2e6" dy=".3em">Small</text>
-      <text x="35%" y="65%" fill="#dee2e6" dy=".3em">2px</text>
+      <text x="50%" y="40%" fill="#dee2e6" dy=".3em">Small</text>
+      <text x="50%" y="65%" fill="#dee2e6" dy=".3em">2px</text>
     </svg>
   </div>
   <br>
   <div>
-    <svg class="bd-placeholder-img rounded-lg" width="75" height="75" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Example small rounded image: 75x75">
+    <svg class="bd-placeholder-img rounded-2" width="75" height="75" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Example small rounded image: 75x75">
+      <title>Example default rounded image</title>
+      <rect width="100%" height="100%" fill="#868e96"></rect>
+      <text x="50%" y="40%" fill="#dee2e6" dy=".3em">Default</text>
+      <text x="50%" y="65%" fill="#dee2e6" dy=".3em">4px</text>
+    </svg>
+  </div>
+  <br>
+  <div>
+    <svg class="bd-placeholder-img rounded-3" width="75" height="75" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Example small rounded image: 75x75">
       <title>Example large rounded image</title>
       <rect width="100%" height="100%" fill="#868e96"></rect>
-      <text x="25%" y="40%" fill="#dee2e6" dy=".3em">Large</text>
-      <text x="35%" y="65%" fill="#dee2e6" dy=".3em">2px</text>
+      <text x="50%" y="40%" fill="#dee2e6" dy=".3em">Large</text>
+      <text x="50%" y="65%" fill="#dee2e6" dy=".3em">3.008px</text>
     </svg>
   </div>
   <br>
   <div>
-    <svg class="bd-placeholder-img rounded-xl" width="75" height="75" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Example small rounded image: 75x75">
-      <title>Example extra large rounded image</title>
+    <svg class="bd-placeholder-img rounded-circle" width="75" height="75" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Example small rounded image: 75x75">
+      <title>Example circle rounded image</title>
       <rect width="100%" height="100%" fill="#868e96"></rect>
-      <text x="15%" y="40%" fill="#dee2e6" dy=".3em">XLarge</text>
-      <text x="35%" y="65%" fill="#dee2e6" dy=".3em">6px</text>
+      <text x="50%" y="40%" fill="#dee2e6" dy=".3em">Circle</text>
+      <text x="50%" y="65%" fill="#dee2e6" dy=".3em">50%</text>
+    </svg>
+  </div>
+  <br>
+  <div>
+    <svg class="bd-placeholder-img rounded-pill" width="75" height="75" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Example small rounded image: 75x75">
+      <title>Example pill rounded image</title>
+      <rect width="100%" height="100%" fill="#868e96"></rect>
+      <text x="50%" y="40%" fill="#dee2e6" dy=".3em">Pill</text>
+      <text x="50%" y="65%" fill="#dee2e6" dy=".3em">50%</text>
     </svg>
   </div>
 </div>`;
@@ -53,13 +71,15 @@ BorderRadius.parameters = {
   notes: `
   ##Sizes
 
-  Default (.rounded) - .25rem / 4px
+  Small (.rounded-1) - .125rem / 2px
 
-  Small (.rounded-sm) - .125rem / 2px
+  Default (.rounded-2) - .25rem / 4px
 
-  Large (.rounded-lg) - .188rem / 3.008px
+  Large (.rounded-3) - .188rem / 3.008px
 
-  Xlarge (.rounded-xl) - .375rem / 6px
+  Circle (.rounded-circle) - 50%
+
+  Pill (.rounded-pill) - 50%
 
   ##Sample Code
   
