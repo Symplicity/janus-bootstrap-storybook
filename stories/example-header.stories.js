fed90cc897b0d7e9bef4fd45d42c61e04bc7b878
Christian Boyle <mail@christianboyle.com>
2021-01-19 15:22:20 -0600

    Add Examples > Layout > Header story, part 2

--- stories/example-header.stories.js
+++ stories/example-header.stories.js
@@ -37,75 +37,77 @@ For additional documentation, see:
 `;
 
 let header_html = `
-<nav class="d-block navbar navbar-light bg-light">
-  <div class="d-flex justify-content-between align-items-center">
-    <div>
-      <button class="border-0 navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <svg width="1rem" height="1rem">
-          <use xlink:href="${iconSprite}#tabler-menu-2" />
-        </svg>
-      </button>
-      <svg class="bd-placeholder-img" width="170" height="64" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">170x64</text></svg>
-    </div>
-    <div>
-      <div class="input-group">
-        <span class="input-group-text">$</span>
-        <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-        <span class="input-group-text">$</span>
+<div class="d-block bg-light">
+  <nav class="d-block navbar navbar-light" style="max-width: 1400px; margin: 0 auto;">
+    <div class="d-flex justify-content-between align-items-center">
+      <div class="d-flex">
+        <button class="border-0 navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <svg width="1rem" height="1rem">
+            <use xlink:href="${iconSprite}#tabler-menu-2" />
+          </svg>
+        </button>
+        <svg class="bd-placeholder-img" width="170" height="64" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">170x64</text></svg>
+      </div>
+      <div class="d-flex">
+        <div class="input-group">
+          <span class="input-group-text">$</span>
+          <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+          <span class="input-group-text">$</span>
+        </div>
+      </div>
+      <div class="d-flex">
+        <button class="btn btn-action btn-action-icon-only btn-action-header me-7" type="button">
+          <svg class="icon-lg-font-size mt-n1">
+            <use xlink:href="${iconSprite}#tabler-star" />
+            <span class="visually-hidden">More Actions</span>
+          </svg>
+        </button>
+        <button class="btn btn-action btn-action-icon-only btn-action-header me-7" type="button">
+          <svg class="icon-lg-font-size mt-n1">
+            <use xlink:href="${iconSprite}#tabler-pencil" />
+            <span class="visually-hidden">More Actions</span>
+          </svg>
+        </button>
+        <button type="button" class="btn btn-action btn-action-icon-only btn-action-header me-7 me-lg-9" data-bs-toggle="dropdown" aria-expanded="false" id="dropdownMenuButton1" data-bs-toggle="dropdown">
+          <svg class="icon-lg-font-size mt-n1">
+            <use xlink:href="${iconSprite}#tabler-dots" />
+            <span class="visually-hidden">More Actions</span>
+          </svg>
+        </button>
+        <ul class="dropdown-menu dropdown-menu-end shadow-sm mt-1" aria-labelledby="dropdownMenuButton1" >
+          <li>
+            <a class="dropdown-item d-flex" href="#">
+              Add to Career Portfolio
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item d-flex" href="#">
+              Remove QR Code
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item d-flex" href="#">
+              View as Word
+            </a>
+          </li>
+          <li><hr class="dropdown-divider"></li>
+          <li>
+            <a class="dropdown-item d-flex text-danger" href="#">
+              Delete
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
-    <div>
-      <button class="btn btn-action btn-action-icon-only" type="button">
-        <svg class="icon-lg-font-size mt-n1">
-          <use xlink:href="${iconSprite}#tabler-star" />
-          <span class="visually-hidden">More Actions</span>
-        </svg>
-      </button>
-      <button class="btn btn-action btn-action-icon-only" type="button">
-        <svg class="icon-lg-font-size mt-n1">
-          <use xlink:href="${iconSprite}#tabler-pencil" />
-          <span class="visually-hidden">More Actions</span>
-        </svg>
-      </button>
-      <button type="button" class="btn btn-action btn-action-icon-only" data-bs-toggle="dropdown" aria-expanded="false" id="dropdownMenuButton1" data-bs-toggle="dropdown">
-        <svg class="icon-lg-font-size mt-n1">
-          <use xlink:href="${iconSprite}#tabler-dots" />
-          <span class="visually-hidden">More Actions</span>
-        </svg>
-      </button>
-      <ul class="dropdown-menu dropdown-menu-end shadow-sm mt-1" aria-labelledby="dropdownMenuButton1" >
-        <li>
-          <a class="dropdown-item d-flex" href="#">
-            Add to Career Portfolio
-          </a>
-        </li>
-        <li>
-          <a class="dropdown-item d-flex" href="#">
-            Remove QR Code
-          </a>
-        </li>
-        <li>
-          <a class="dropdown-item d-flex" href="#">
-            View as Word
-          </a>
-        </li>
-        <li><hr class="dropdown-divider"></li>
-        <li>
-          <a class="dropdown-item d-flex text-danger" href="#">
-            Delete
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+  </nav>
+</div>
 `;
 
 
 
 export const story1 = () => { 
 
-  const container = $('<div class="text-center" style="margin-top:100px"></div>').append(header_html);
+  const container = $('<div style="margin: 100px -1rem 0;"></div>').append(header_html);
  
   /*
   setTimeout(function() {
