a882741ad1500e43b40d114e0b450aed383eb315
Christian Boyle <christianboyle@users.noreply.github.com>
2021-02-02 13:25:56 -0600

    Add Search > Mobile Combo Input

--- stories/search.stories.js
+++ stories/search.stories.js
@@ -103,7 +103,7 @@ export const SearchMobileInput = () => {
   return container[0];
 };
 
-SearchMobileInput.storyName = 'Search Mobile Input';
+SearchMobileInput.storyName = 'Mobile Input';
 
 SearchMobileInput.parameters = { 
   notes: `
@@ -112,6 +112,77 @@ SearchMobileInput.parameters = {
 
   \`\`\`html
   ${search_mobile_input_html}
+  \`\`\`
+
+  `
+};
+
+
+const search_mobile_combo_input_html = `
+<div class="m-3">
+  <button data-bs-toggle="modal" data-bs-target="#myModal" class="rounded-circle btn btn-action btn-action-icon-only btn-action-header me-7" type="button">
+    <svg class="icon-xl-font-size mt-n1">
+      <use xlink:href="${iconSprite}#tabler-search" />
+      <span class="visually-hidden">Open Search Dialog</span>
+    </svg>
+  </button>
+</div>
+
+<div class="modal" tabindex="-1" id="myModal">
+  <div class="modal-dialog modal-fullscreen-lg-down" role="document">
+    <div class="modal-content">
+      <div class="modal-header bg-white">
+        <h4 class="modal-title visually-hidden">Search</h4>
+        <button type="button" class="btn-close d-flex" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body align-items-start justify-content-center">
+        <!-- input -->
+        <select class="form-select rounded-pill border-0 shadow-lg mb-7" aria-label="Search input">
+          <option selected>search in...</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+        <div class="input-group flex-nowrap rounded-pill shadow-lg overflow-hidden">
+          <input type="text" id="myInput" class="form-control border-0" aria-label="Search input" aria-describedby="addon-wrapping">
+          <span class="input-group-text border-0" id="addon-wrapping">
+            <svg class="icon-lg-font-size mt-n1">
+              <use xlink:href="${iconSprite}#tabler-search" />
+              <span class="visually-hidden">Open Search Dialog</span>
+            </svg>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`;
+
+export const SearchMobileComboInput = () => {
+  const container = $('<div style="margin: 0;"></div>').append(search_mobile_combo_input_html)
+
+  setTimeout(function() {
+    /* Mimic autofocus attribute in bs.modal: http://localhost:9001/docs/5.0/components/modal/#how-it-works */
+
+    var myModal = document.getElementById('myModal')
+    var myInput = document.getElementById('myInput')
+  
+    myModal.addEventListener('shown.bs.modal', function () {
+      myInput.focus()
+    })
+  },0)
+
+  return container[0];
+};
+
+SearchMobileComboInput.storyName = 'Mobile Combo Input';
+
+SearchMobileComboInput.parameters = { 
+  notes: `
+  
+  #Sample Code
+
+  \`\`\`html
+  ${search_mobile_combo_input_html}
   \`\`\`
 
   `
