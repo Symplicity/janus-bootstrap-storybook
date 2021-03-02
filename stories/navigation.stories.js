64b780015fd6e0a2bd058a087249e3cd9a8035b4
Christian Boyle <mail@christianboyle.com>
2021-03-02 10:31:00 -0600

    Fix header/nav markup, add handling for responsive 100vh nav dropdown

--- stories/navigation.stories.js
+++ stories/navigation.stories.js
@@ -37,8 +37,8 @@ For additional documentation, see:
 `;
 
 const navigation_html = `
-    <div class="d-block bg-white m-0">
-      <nav class="d-block navbar navbar-light shadow-lg" style="max-width: 1400px; margin: 0 auto; background: #fff">
+    <div class="d-block bg-white m-0 pt-0">
+      <header class="main-nav d-block navbar navbar-light shadow-lg" style="max-width: 1400px; margin: 0 auto; background: #fff">
         <div class="d-flex justify-content-between align-items-center">
           <div class="d-flex align-items-center">
             <button class="border-0 navbar-toggler btn btn-action btn-action-icon-only btn-action-header ms-8 me-7" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -49,16 +49,16 @@ const navigation_html = `
             <svg class="bd-placeholder-img d-none d-md-block" width="170" height="64" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">170x64</text></svg>
           </div>
         </div>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav p-0">
-            <li class="nav-item">
-              <a class="nav-link p-7 w-100 active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link p-7 w-100" href="#">Link</a>
-            </li>
-          </ul>
-        </div>
+      </header>
+      <nav class="main-nav collapse navbar-collapse bg-light mt-0" id="navbarNav">
+        <ul class="navbar-nav p-0">
+          <li class="nav-item">
+            <a class="nav-link p-7 w-100 active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link p-7 w-100" href="#">Link</a>
+          </li>
+        </ul>
       </nav>
     </div>
 `
