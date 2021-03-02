2a62e622dad77b47674f85c758cd0f5e17c3e9d3
Christian Boyle <mail@christianboyle.com>
2021-03-02 13:12:10 -0600

    Fix main nav item styling

--- stories/navigation.stories.js
+++ stories/navigation.stories.js
@@ -37,8 +37,8 @@ For additional documentation, see:
 `;
 
 const navigation_html = `
-    <div class="d-block bg-white m-0 pt-0">
-      <header class="main-nav d-block navbar navbar-light shadow-lg" style="max-width: 1400px; margin: 0 auto; background: #fff">
+    <div class="d-block bg-white pt-0" style="max-width: 1400px; margin: 0 auto; background: #fff">
+      <header class="main-nav d-block navbar navbar-light shadow-lg">
         <div class="d-flex justify-content-between align-items-center">
           <div class="d-flex align-items-center">
             <button class="border-0 navbar-toggler btn btn-action btn-action-icon-only btn-action-header ms-8 me-7" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -51,12 +51,30 @@ const navigation_html = `
         </div>
       </header>
       <nav class="main-nav collapse navbar-collapse bg-light mt-0" id="navbarNav">
-        <ul class="navbar-nav p-0">
+        <ul class="navbar-nav pt-4 pb-4">
           <li class="nav-item">
-            <a class="nav-link p-7 w-100 active" aria-current="page" href="#">Home</a>
+            <a class="nav-link text-dark ps-3 pe-4 pt-2 pb-2 h-auto active" aria-current="page" href="#">
+              <span>
+                <svg width="1rem" height="1rem" class="me-2 mt-n1">
+                  <use xlink:href="${iconSprite}#tabler-home" />
+                </svg>
+              </span>
+              <span class="flex-fill">
+                Home
+              </span>
+            </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link p-7 w-100" href="#">Link</a>
+            <a class="nav-link text-dark ps-4 pe-4 pt-2 pb-2 h-auto" href="#">
+              <span>
+                <svg width="1rem" height="1rem" class="me-2 mt-n1">
+                  <use xlink:href="${iconSprite}#tabler-briefcase" />
+                </svg>
+              </span>
+              <span class="flex-fill">
+                Jobs
+              </span>
+            </a>
           </li>
         </ul>
       </nav>
