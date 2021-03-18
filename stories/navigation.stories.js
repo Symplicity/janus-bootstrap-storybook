eb9f748907e00b0ed5bd1fa7f32f7ff03b6e5455
Christian Boyle <mail@christianboyle.com>
2021-03-18 11:36:21 -0500

    Fix desktop main nav item height, per spec

--- stories/navigation.stories.js
+++ stories/navigation.stories.js
@@ -55,9 +55,9 @@ const desktop_navigation_html = `
         </div>
       </nav>
       <nav class="main-nav navbar-collapse show mt-0 fs-5" id="navbarNav">
-        <ul class="navbar-nav pt-4 pb-0">
+        <ul class="navbar-nav pt-2 pb-0">
           <li class="nav-item">
-            <a class="nav-link text-dark ms-4 me-4 mt-2 mb-2 h-auto active" aria-current="page" href="#">
+            <a class="nav-link text-dark ms-4 me-4 mt-10 mb-10 h-auto active" aria-current="page" href="#">
               <span>
                 <svg width="1rem" height="1rem" class="me-2 mt-n1">
                   <use xlink:href="${iconSprite}#tabler-home" />
@@ -69,7 +69,7 @@ const desktop_navigation_html = `
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-dark ms-4 me-4 mt-2 mb-2 h-auto" href="#">
+            <a class="nav-link text-dark ms-4 me-4 mt-10 mb-10 h-auto" href="#">
               <span>
                 <svg width="1rem" height="1rem" class="me-2 mt-n1">
                   <use xlink:href="${iconSprite}#tabler-briefcase" />
@@ -82,7 +82,7 @@ const desktop_navigation_html = `
           </li>
           <li class="nav-item">
             <div class="list-group">
-              <a class="d-flex nav-link text-dark ms-4 me-4 mt-2 mb-2 h-auto text-decoration-none" type="button" id="dropdownMenu2" data-bs-toggle="collapse" data-bs-target="#navbarNav2" aria-controls="navbarNav2" aria-expanded="false" aria-label="Toggle navigation" href="#">
+              <a class="d-flex nav-link text-dark ms-4 me-4 mt-10 mb-10 h-auto text-decoration-none" type="button" id="dropdownMenu2" data-bs-toggle="collapse" data-bs-target="#navbarNav2" aria-controls="navbarNav2" aria-expanded="false" aria-label="Toggle navigation" href="#">
                 <span>
                   <svg width="1rem" height="1rem" class="me-2 mt-n1">
                     <use xlink:href="${iconSprite}#tabler-calendar" />
@@ -99,7 +99,7 @@ const desktop_navigation_html = `
               </a>
               <ul class="navbar-nav collapse ps-2" aria-labelledby="dropdownMenu2" id="navbarNav2">
                 <li class="nav-item">
-                  <a class="nav-link text-dark ps-3 pe-4 pt-2 pb-2 h-auto" aria-current="page" href="#">
+                  <a class="nav-link text-dark ms-3 me-4 mt-10 mb-10 h-auto" aria-current="page" href="#">
                     <span>
                       <svg width="1rem" height="1rem" class="me-2 mt-n1">
                         <use xlink:href="${iconSprite}#tabler-point-on" />
@@ -111,7 +111,7 @@ const desktop_navigation_html = `
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link text-dark ps-3 pe-4 pt-2 pb-2 h-auto" aria-current="page" href="#">
+                  <a class="nav-link text-dark ms-3 me-4 mt-10 mb-10 h-auto" aria-current="page" href="#">
                     <span>
                       <svg width="1rem" height="1rem" class="me-2 mt-n1">
                         <use xlink:href="${iconSprite}#tabler-point-on" />
@@ -123,7 +123,7 @@ const desktop_navigation_html = `
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link text-dark ps-3 pe-4 pt-2 pb-2 h-auto active" aria-current="page" href="#">
+                  <a class="nav-link text-dark ms-3 me-4 mt-10 mb-10 h-auto active" aria-current="page" href="#">
                     <span>
                       <svg width="1rem" height="1rem" class="me-2 mt-n1">
                         <use xlink:href="${iconSprite}#tabler-point-on" />
@@ -141,7 +141,7 @@ const desktop_navigation_html = `
         <hr class="ms-4 me-4 mt-2 mb-2">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link text-dark ms-4 me-4 mt-2 mb-2 h-auto" href="#">
+            <a class="nav-link text-dark ms-4 me-4 mt-10 mb-10 h-auto" href="#">
               <span>
                 <svg width="1rem" height="1rem" class="me-2 mt-n1">
                   <use xlink:href="${iconSprite}#tabler-home" />
@@ -153,7 +153,7 @@ const desktop_navigation_html = `
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-dark ms-4 me-4 mt-2 mb-2 h-auto" href="#">
+            <a class="nav-link text-dark ms-4 me-4 mt-10 mb-10 h-auto" href="#">
               <span>
                 <svg width="1rem" height="1rem" class="me-2 mt-n1">
                   <use xlink:href="${iconSprite}#tabler-home" />
@@ -168,7 +168,7 @@ const desktop_navigation_html = `
         <hr class="ms-4 me-4 mt-2 mb-2">
         <ul class="navbar-nav pb-4">
           <li class="nav-item">
-            <a class="nav-link text-dark ms-4 me-4 mt-2 mb-2 h-auto" href="#">
+            <a class="nav-link text-dark ms-4 me-4 mt-10 mb-10 h-auto" href="#">
               <span>
                 <svg width="1rem" height="1rem" class="me-2 mt-n1">
                   <use xlink:href="${iconSprite}#tabler-home" />
@@ -180,7 +180,7 @@ const desktop_navigation_html = `
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-dark ms-4 me-4 mt-2 mb-2 h-auto" href="#">
+            <a class="nav-link text-dark ms-4 me-4 mt-10 mb-10 h-auto" href="#">
               <span>
                 <svg width="1rem" height="1rem" class="me-2 mt-n1">
                   <use xlink:href="${iconSprite}#tabler-home" />
