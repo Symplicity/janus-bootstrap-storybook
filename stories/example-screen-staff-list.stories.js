d04433909c5ff80b0b4eaea33f75840d8fd50f84
Zamur Borges <zamur@Zamurs-Mac-mini.local>
2021-06-21 15:55:06 -0300

    starting w staff list content

--- stories/example-screen-staff-list.stories.js
+++ stories/example-screen-staff-list.stories.js
@@ -108,67 +108,72 @@ export const ScreenStaffList = () => {
   );
 
   return `
-    <header class="d-block">
-      <nav class="d-block navbar navbar-light bg-white shadow-lg" style="max-width: 1400px; margin: 0 auto; background: ${header_bg_color}">
-        <div class="d-flex justify-content-between align-items-center">
-          <div class="d-flex align-items-center">
-            <button class="main-nav-trigger border-0 navbar-toggler btn btn-action btn-action-icon-only btn-action-header ms-7 me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <svg width="1rem" height="1rem">
-                <use xlink:href="${iconSprite}#tabler-menu-2" />
-              </svg>
-            </button>
-            <svg class="bd-placeholder-img d-none d-md-block" width="170" height="64" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">170x64</text></svg>
-          </div>
-          <div class="d-flex">
-            <div class="input-group d-none d-md-flex">
-              <span class="input-group-text">$</span>
-              <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-              <span class="input-group-text">$</span>
+    <section>
+      <header class="d-block">
+        <nav class="d-block navbar navbar-light bg-white shadow-lg" style="max-width: 1400px; margin: 0 auto; background: ${header_bg_color}">
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="d-flex align-items-center">
+              <button class="main-nav-trigger border-0 navbar-toggler btn btn-action btn-action-icon-only btn-action-header ms-7 me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <svg width="1rem" height="1rem">
+                  <use xlink:href="${iconSprite}#tabler-menu-2" />
+                </svg>
+              </button>
+              <svg class="bd-placeholder-img d-none d-md-block" width="170" height="64" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">170x64</text></svg>
+            </div>
+            <div class="d-flex">
+              <div class="input-group d-none d-md-flex">
+                <span class="input-group-text">$</span>
+                <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+                <span class="input-group-text">$</span>
+              </div>
+            </div>
+            <div class="d-flex me-7 me-md-0">
+              <button class="rounded-circle btn btn-action btn-action-icon-only btn-action-header me-7" type="button">
+                <svg class="icon-xl-font-size mt-n1">
+                  <use xlink:href="${iconSprite}#tabler-star" />
+                  <span class="visually-hidden">More Actions</span>
+                </svg>
+              </button>
+              <button class="rounded-circle btn btn-action btn-action-icon-only btn-action-header me-7" type="button">
+                <svg class="icon-xl-font-size mt-n1">
+                  <use xlink:href="${iconSprite}#tabler-pencil" />
+                  <span class="visually-hidden">More Actions</span>
+                </svg>
+              </button>
+              <button type="button" class="btn btn-action btn-action-icon-only btn-action-header rounded-circle border border-dark w-auto h-auto me-1 me-md-9"  data-bs-toggle="dropdown" aria-expanded="false" id="dropdownMenuButton1">
+                <img role="button" class="rounded-circle border border-white icon-xxl-font-size" src="https://www.edge.org/sites/default/files/styles/member-photo/public/member-pictures/picture-510-1416068804.jpg?itok=cnAQyDr5" alt="...">
+              </button>
+              <ul class="dropdown-menu dropdown-menu-end shadow-sm mt-1" aria-labelledby="dropdownMenuButton1" >
+                <li>
+                  <a class="dropdown-item d-flex" href="#">
+                    Add to Career Portfolio
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item d-flex" href="#">
+                    Remove QR Code
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item d-flex" href="#">
+                    View as Word
+                  </a>
+                </li>
+                <li><hr class="dropdown-divider"></li>
+                <li>
+                  <a class="dropdown-item d-flex text-danger" href="#">
+                    Delete
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
-          <div class="d-flex me-7 me-md-0">
-            <button class="rounded-circle btn btn-action btn-action-icon-only btn-action-header me-7" type="button">
-              <svg class="icon-xl-font-size mt-n1">
-                <use xlink:href="${iconSprite}#tabler-star" />
-                <span class="visually-hidden">More Actions</span>
-              </svg>
-            </button>
-            <button class="rounded-circle btn btn-action btn-action-icon-only btn-action-header me-7" type="button">
-              <svg class="icon-xl-font-size mt-n1">
-                <use xlink:href="${iconSprite}#tabler-pencil" />
-                <span class="visually-hidden">More Actions</span>
-              </svg>
-            </button>
-            <button type="button" class="btn btn-action btn-action-icon-only btn-action-header rounded-circle border border-dark w-auto h-auto me-1 me-md-9"  data-bs-toggle="dropdown" aria-expanded="false" id="dropdownMenuButton1">
-              <img role="button" class="rounded-circle border border-white icon-xxl-font-size" src="https://www.edge.org/sites/default/files/styles/member-photo/public/member-pictures/picture-510-1416068804.jpg?itok=cnAQyDr5" alt="...">
-            </button>
-            <ul class="dropdown-menu dropdown-menu-end shadow-sm mt-1" aria-labelledby="dropdownMenuButton1" >
-              <li>
-                <a class="dropdown-item d-flex" href="#">
-                  Add to Career Portfolio
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item d-flex" href="#">
-                  Remove QR Code
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item d-flex" href="#">
-                  View as Word
-                </a>
-              </li>
-              <li><hr class="dropdown-divider"></li>
-              <li>
-                <a class="dropdown-item d-flex text-danger" href="#">
-                  Delete
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
+      <div class="main-content">
+        <!-- add content here -->
+      </div>
+    </section>
   `;
 };
 ScreenStaffList.storyName = 'Staff List';
