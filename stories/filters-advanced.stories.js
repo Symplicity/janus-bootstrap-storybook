6538eab3bcddc6e6cd11807aadf0c748fe786490
Christian Boyle <mail@christianboyle.com>
2021-08-16 11:37:28 -0500

    Fix static Notes version of Filters Advanced to match dynamic version

--- stories/filters-advanced.stories.js
+++ stories/filters-advanced.stories.js
@@ -22,30 +22,56 @@ export default {
 
 const filter_button_html = `
 <div class="dropdown">
-  <h3>Button with Dropdown</h3>
-  <div>
-    <button class="btn btn-outline-secondary filter-toggle rounded-pill" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      All Jobs and Interviews
-    </button>
-    <div class="dropdown-menu shadow-sm mt-1" aria-labelledby="dropdownMenuButton2">
-      <a class="dropdown-item d-flex" href="#">
-        <span class="flex-fill">
-          All Jobs and Interviews
-        </span>
-      </a>
-      <a class="dropdown-item d-flex" href="#">
-        <span class="flex-fill">
-          Career Fair Postings
-        </span>
-      </a>
-      <a class="dropdown-item d-flex" href="#">
-        <span class="flex-fill">
-          Jobs Matching My Profile
-        </span>
-      </a>
-    </div>
-  </div>
-</div>`;
+      <h3>Scrollable, Multi-Level, Single-Select Filter</h3>
+      <div>
+        <button class="btn btn-outline-secondary filter-toggle rounded-pill" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Position Type 
+        </button>
+        <div class="dropdown-menu shadow-sm mt-1 pb-0 pt-0" aria-labelledby="dropdownMenuButton2">
+          <form class="py-3 px-7 mb-0 bg-white border-bottom-light">
+            <div class="input-group">
+              <input type="search" class="form-control border-end-0" autocomplete="false" placeholder="Type to search">
+              <span class="input-group-text bg-white">
+                  <svg class="icon-lg-font-size">
+                    <use xlink:href="/path/to/icon/sprite/#tabler-search" />
+                    <span class="visually-hidden">Search</span>
+                  </svg>
+              </span>
+            </div>
+          </form>
+          <ul class="list-unstyled mb-0 overflow-auto">
+            <li>
+              <div class="list-group">
+                <a class="dropdown-item d-flex" href="#">
+                  <span class="w-100">
+                    All Jobs and Interviews
+                  </span>
+                  <span class="h4 mt-0 mb-0 me-n2">
+                    <svg width="1.25rem" height="1.25rem">
+                      <use xlink:href="/path/to/icon/sprite/#tabler-chevron-right" />
+                    </svg>
+                  </span>
+                </a>
+            </li>
+            <li>
+              <a class="dropdown-item d-flex" href="#">
+                <span class="flex-fill">
+                  Career Fair Postings
+                </span>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item d-flex" href="#">
+                <span class="flex-fill">
+                  Jobs Matching My Profile
+                </span>
+              </a>
+            </li>
+            <ul class="list-unstyled list-unstyled-repeat mb-0"></ul>
+          </ul>
+        </div>
+      </div>
+    </div>`;
 
 export const PicklistButton = () => {
   const filter_option_1 = text('Select Option 1', 'All Jobs and Interviews');
@@ -104,7 +130,7 @@ export const PicklistButton = () => {
                   <span class="w-100">
                     ${filter_option_1}
                   </span>
-                  <span class="tabler-chevron-down h4 mt-0 mb-0 me-n2">
+                  <span class="h4 mt-0 mb-0 me-n2">
                     <svg width="1.25rem" height="1.25rem">
                       <use xlink:href="${iconSprite}#tabler-chevron-right" />
                     </svg>
