6609e7b0d9574b9d3bc18783c06b8872450be0b6
Christian Boyle <mail@christianboyle.com>
2021-08-17 10:22:41 -0500

    Add Filters Advanced > Drilldown Child story, rename Filters Advanced stories to Drilldown Parent/Child

--- stories/filters-advanced.stories.js
+++ stories/filters-advanced.stories.js
@@ -21,8 +21,8 @@ export default {
 };
 
 const filter_button_html = `
-<div class="dropdown">
-      <h3>Scrollable, Multi-Level, Single-Select Filter</h3>
+    <div class="dropdown">
+      <h3>Scrollable, Drilldown, Single-Select Filter</h3>
       <div>
         <button class="btn btn-outline-secondary filter-toggle rounded-pill" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Position Type 
@@ -106,7 +106,7 @@ export const PicklistButton = () => {
 
   return `
     <div class="dropdown">
-      <h3>Scrollable, Multi-Level, Single-Select Filter</h3>
+      <h3>Drilldown Parent</h3>
       <div>
         <button class="btn btn-outline-secondary filter-toggle rounded-pill" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Position Type 
@@ -159,7 +159,7 @@ export const PicklistButton = () => {
     `;
 };
 
-PicklistButton.storyName = 'Multi-Level, Single-Select';
+PicklistButton.storyName = 'Drilldown Parent';
 
 PicklistButton.parameters = {
   notes: `
@@ -168,6 +168,147 @@ PicklistButton.parameters = {
 
   \`\`\`html
   ${filter_button_html}
+  \`\`\`
+
+  `,
+};
+
+const filter_button_nested_html = `
+    <div class="dropdown">
+      <h3>Drilldown Child</h3>
+      <div>
+        <button class="btn btn-outline-secondary filter-toggle rounded-pill" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Position Type 
+        </button>
+        <div class="dropdown-menu shadow-sm mt-1 pb-0 pt-0" aria-labelledby="dropdownMenuButton2">
+          <form class="py-3 px-7 mb-0 bg-white border-bottom-light">
+            <div class="input-group">
+              <input type="search" class="form-control border-end-0" autocomplete="false" placeholder="Type to search">
+              <span class="input-group-text bg-white">
+                  <svg class="icon-lg-font-size">
+                    <use xlink:href="/path/to/icon/sprite/#tabler-search" />
+                    <span class="visually-hidden">Search</span>
+                  </svg>
+              </span>
+            </div>
+          </form>
+          <ul class="list-unstyled mb-0 overflow-auto">
+            <li>
+              <div class="list-group">
+                <a class="dropdown-item d-flex" href="#">
+                  <span class="w-100">
+                    All Jobs and Interviews
+                  </span>
+                  <span class="h4 mt-0 mb-0 me-n2">
+                    <svg width="1.25rem" height="1.25rem">
+                      <use xlink:href="/path/to/icon/sprite/#tabler-chevron-right" />
+                    </svg>
+                  </span>
+                </a>
+            </li>
+            <li>
+              <a class="dropdown-item d-flex" href="#">
+                <span class="flex-fill">
+                  Career Fair Postings
+                </span>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item d-flex" href="#">
+                <span class="flex-fill">
+                  Jobs Matching My Profile
+                </span>
+              </a>
+            </li>
+            <ul class="list-unstyled list-unstyled-repeat mb-0"></ul>
+          </ul>
+        </div>
+      </div>
+    </div>`;
+
+export const PicklistButtonNested = () => {
+  const filter_nested_option_1 = text('Select Option 1', 'Category 1.3.1');
+  const filter_nested_option_2 = text('Select Option 2', 'Category 1.3.2');
+  const filter_nested_option_3 = text('Select Option 3', 'Category 1.3.3');
+
+  return `
+    <div class="dropdown">
+      <h3>Scrollable, Drilldown, Single-Select Filter</h3>
+      <div>
+        <button class="btn btn-outline-secondary filter-toggle rounded-pill" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Position Type 
+        </button>
+        <div class="dropdown-menu shadow-sm mt-1 pb-0 pt-0" aria-labelledby="dropdownMenuButton2">
+          <form class="py-3 px-7 mb-0 bg-white border-bottom-light">
+            <div class="input-group">
+              <input type="search" class="form-control border-end-0" autocomplete="false" placeholder="Type to search">
+              <span class="input-group-text bg-white">
+                  <svg class="icon-lg-font-size">
+                    <use xlink:href="${iconSprite}#tabler-search" />
+                    <span class="visually-hidden">Search</span>
+                  </svg>
+              </span>
+            </div>
+          </form>
+          <ul class="list-unstyled mb-2 overflow-auto">
+            <li class="mb-0">
+              <div class="list-group">
+                <a class="dropdown-item d-flex border border-2 border-light border-start-0 border-end-0" href="#">
+                  <span class="h4 mt-0 mb-0 ms-n2 ">
+                    <svg width="1.25rem" height="1.25rem">
+                      <use xlink:href="${iconSprite}#tabler-chevron-left" />
+                    </svg>
+                  </span>
+                  <span class="w-100 text-center">
+                    Category 1.3
+                  </span>
+                </a>
+            </li>
+          </ul>
+          <ul class="list-unstyled mb-0 overflow-auto">
+            <li>
+              <div class="list-group">
+                <a class="dropdown-item d-flex" href="#">
+                  <span class="w-100">
+                    ${filter_nested_option_1}
+                  </span>
+                  <span class="h4 mt-0 mb-0 me-n2">
+                    <svg width="1.25rem" height="1.25rem">
+                      <use xlink:href="${iconSprite}#tabler-chevron-right" />
+                    </svg>
+                  </span>
+                </a>
+            </li>
+            <li>
+              <a class="dropdown-item d-flex" href="#">
+                <span class="flex-fill">
+                  ${filter_nested_option_2}
+                </span>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item d-flex" href="#">
+                <span class="flex-fill">
+                  ${filter_nested_option_3}
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    `;
+};
+
+PicklistButtonNested.storyName = 'Drilldown Child';
+
+PicklistButtonNested.parameters = {
+  notes: `
+  
+  #Sample Code
+
+  \`\`\`html
+  ${filter_button_nested_html}
   \`\`\`
 
   `,
