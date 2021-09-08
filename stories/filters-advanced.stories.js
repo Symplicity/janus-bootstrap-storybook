0c2a26f9c94ddc4a3ba5eda9c45d353d3dda52e8
Christian Boyle <mail@christianboyle.com>
2021-09-08 15:24:13 -0500

    Add Filters Advanced > Multi-Select, Single-Level, With Search story

--- stories/filters-advanced.stories.js
+++ stories/filters-advanced.stories.js
@@ -198,7 +198,7 @@ const drilldown_button_child_html = `
           <ul class="list-unstyled mb-2 overflow-auto">
             <li class="mb-0">
               <div class="list-group">
-                <a class="dropdown-item d-flex border border-2 border-light border-start-0 border-end-0" href="#">
+                <a class="dropdown-item d-flex border border-light border-start-0 border-end-0" href="#">
                   <span class="h4 mt-0 mb-0 ms-n2 ">
                     <svg width="1.25rem" height="1.25rem">
                       <use xlink:href="/path/to/icon-sprite.svg#tabler-chevron-left" />
@@ -271,7 +271,7 @@ export const FilterButtonChild = () => {
           <ul class="list-unstyled mb-2 overflow-auto">
             <li class="mb-0">
               <div class="list-group">
-                <a class="dropdown-item d-flex border border-2 border-light border-start-0 border-end-0" href="#">
+                <a class="dropdown-item d-flex border border-light border-start-0 border-end-0" href="#">
                   <span class="h4 mt-0 mb-0 ms-n2 ">
                     <svg width="1.25rem" height="1.25rem">
                       <use xlink:href="${iconSprite}#tabler-chevron-left" />
@@ -435,6 +435,129 @@ MultiSelectSingleLevel.parameters = {
 
   \`\`\`html
   ${multi_select_single_level_html}
+  \`\`\`
+
+  `,
+};
+
+const multi_select_single_level_with_search_html = `
+    <div class="dropdown">
+      <h3>Multi-Select, Single-Level, With Search</h3>
+      <div>
+        <button class="btn btn-outline-secondary filter-toggle rounded-pill" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-bs-auto-close="outside">
+          Position Type 
+        </button>
+        <div class="dropdown-menu filter-menu shadow-sm mt-1 pb-0 pt-0" aria-labelledby="dropdownMenuButton2">
+          <ul class="list-unstyled mb-0 px-7 pt-7 pb-2 overflow-auto">
+            <li>
+              <label class="list-group-item d-flex border-0 p-0 mb-2"><input class="form-check-input me-2" type="checkbox" value="" aria-label="...">Option 1</label>
+            </li>
+            <li>
+              <label class="list-group-item d-flex border-0 p-0 mb-2"><input class="form-check-input me-2" type="checkbox" value="" aria-label="...">Option 1</label>
+            </li>
+            <li>
+              <label class="list-group-item d-flex border-0 p-0 mb-2"><input class="form-check-input me-2" type="checkbox" value="" aria-label="...">Option 1</label>
+            </li>
+          </ul>
+          <div class="dropdown-divider m-0"></div>
+          <div class="d-flex justify-content-between px-3 py-6">
+            <button type="button" class="btn btn-link minw-0 text-secondary text-decoration-none p-0">Clear</button>
+            <button type="button" class="btn btn-primary">Apply</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+
+export const MultiSelectSingleLevelWithSearch = () => {
+  const drilldown_option_1 = text('Select Option 1', 'All Jobs and Interviews');
+  const drilldown_option_2 = text('Select Option 2', 'Career Fair Postings');
+  const drilldown_option_3 = text(
+    'Select Option 3',
+    'Jobs Matching My Profile'
+  );
+
+  const label = 'Number of items';
+  const defaultValue = 15;
+  const options = {
+    range: true,
+    min: 0,
+    max: 100,
+    step: 1,
+  };
+
+  const value = number(label, defaultValue, options);
+
+  setTimeout(function () {
+    const createListItems = document.querySelector('.list-unstyled-repeat');
+    createListItems.innerHTML = `
+        ${Array(value)
+          .fill()
+          .map(
+            (item, i) =>
+              `<li><label class="list-group-item d-flex border-0 p-0 mb-2"><input class="form-check-input me-2" type="checkbox" value="" aria-label="...">Option ${
+                i + 1
+              }</label></li>`
+          )
+          .join('')}
+    `;
+  }, 0);
+
+  return `
+    <div class="dropdown">
+      <h3>Multi-Select, Single-Level, With Search</h3>
+      <div>
+        <button class="btn btn-outline-secondary filter-toggle rounded-pill" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-bs-auto-close="outside">
+          Position Type 
+        </button>
+        <div class="dropdown-menu filter-menu shadow-sm mt-1 pb-0 pt-0" aria-labelledby="dropdownMenuButton2">
+          <form class="py-3 px-7 mb-0 bg-white border-bottom-light">
+              <div class="input-group">
+                <input type="search" class="form-control border-end-0" autocomplete="false" placeholder="Type to search">
+                <span class="input-group-text bg-white">
+                    <svg class="icon-lg-font-size">
+                      <use xlink:href="${iconSprite}#tabler-search" />
+                      <span class="visually-hidden">Search</span>
+                    </svg>
+                </span>
+              </div>
+            </form>
+          <ul class="list-unstyled mb-0 px-7 pt-0 pt-0 pb-0 overflow-auto">
+            <li>
+              <label class="list-group-item d-flex border-0 p-0 mb-2"><input class="form-check-input me-2" type="checkbox" value="" checked aria-label="...">${drilldown_option_1}</label>
+            </li>
+            <li>
+              <label class="list-group-item d-flex border-0 p-0 mb-2"><input class="form-check-input me-2" type="checkbox" value="" checked aria-label="...">${drilldown_option_2}</label>
+            </li>
+          </ul>
+          <div class="dropdown-divider"></div>
+          <ul class="list-unstyled mb-0 px-7 pt-2 pt-0 pb-2 overflow-auto">
+            <li>
+              <label class="list-group-item d-flex border-0 p-0 mb-2"><input class="form-check-input me-2" type="checkbox" value="" aria-label="...">${drilldown_option_3}</label>
+            </li>
+            <ul class="list-unstyled list-unstyled-repeat list-group mb-0"></ul>
+          </ul>
+          <div class="dropdown-divider m-0"></div>
+          <div class="d-flex justify-content-between px-3 py-6">
+            <button type="button" class="btn btn-link minw-0 text-secondary text-decoration-none p-0">Clear</button>
+            <button type="button" class="btn btn-primary">Apply</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+};
+
+MultiSelectSingleLevelWithSearch.storyName =
+  'Multi-Select, Single-Level, With Search';
+
+MultiSelectSingleLevelWithSearch.parameters = {
+  notes: `
+  
+  #Sample Code
+
+  \`\`\`html
+  ${multi_select_single_level_with_search_html}
   \`\`\`
 
   `,
