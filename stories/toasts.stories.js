03a31532c55ac8f2ff1edb26803f85c7924d40ef
Christian Boyle <mail@christianboyle.com>
2022-01-11 13:56:29 -0600

    update toast stories with templating to reduce code duplication

--- stories/toasts.stories.js
+++ stories/toasts.stories.js
@@ -61,12 +61,12 @@ ToastLive.parameters = {
   `
 }
 
-const toast_success_html = `
+const toast_html = `
 <div class="toast align-items-center text-white bg-success border-0 fade show" role="alert" aria-live="assertive" aria-atomic="true" style="max-width: fit-content">
   <div class="toast-body d-flex">
     <div class="flex-shrink-1 d-flex">
       <svg width="1.5rem" height="1.5rem" class="me-2">
-        <use xlink:href="${iconSprite}#tabler-circle-check" />
+        <use xlink:href="/path/to/icon-sprite.svg#tabler-icon-name" />
       </svg>
     </div>
     <div class="flex-grow-1 me-3">
@@ -83,7 +83,7 @@ const toast_success_html = `
   <div class="toast-body d-flex">
     <div class="flex-shrink-1 d-flex">
       <svg width="1.5rem" height="1.5rem" class="me-2">
-        <use xlink:href="${iconSprite}#tabler-circle-check" />
+        <use xlink:href="/path/to/icon-sprite.svg#tabler-icon-name" />
       </svg>
     </div>
     <div class="flex-grow-1 me-3">
@@ -107,7 +107,7 @@ const toast_success_html = `
   <div class="toast-body d-flex">
     <div class="flex-shrink-1 d-flex">
       <svg width="1.5rem" height="1.5rem" class="me-2">
-        <use xlink:href="${iconSprite}#tabler-circle-check" />
+        <use xlink:href="/path/to/icon-sprite.svg#tabler-icon-name" />
       </svg>
     </div>
     <div class="flex-grow-1">
@@ -128,20 +128,185 @@ const toast_success_html = `
 </div>
 `
 
-export const ToastSuccess = () => `${toast_success_html}`
+const Toasts = (args) => `
+<div class="toast align-items-center text-white bg-${
+  args.class
+} border-0 fade show" role="alert" aria-live="assertive" aria-atomic="true" style="max-width: fit-content">
+  <div class="toast-body d-flex">
+    <div class="flex-shrink-1 ${args.show ? `d-flex` : `d-none`}">
+      <svg width="1.5rem" height="1.5rem" class="me-2">
+        <use xlink:href="${iconSprite}#tabler-${args.icon}" />
+      </svg>
+    </div>
+    <div class="flex-grow-1 me-3">
+      <p class="mb-0">File updated.</p>
+    </div>
+    <div class="flex-shrink-1 mt-n1 mb-n1">
+      <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast" aria-label="Close">
+      </button>
+    </div>
+  </div>
+</div>
 
-ToastSuccess.storyName = 'Success'
+<div class="toast align-items-center text-white bg-${
+  args.class
+} border-0 fade show" role="alert" aria-live="assertive" aria-atomic="true" style="max-width: fit-content">
+  <div class="toast-body d-flex">
+    <div class="flex-shrink-1 ${args.show ? `d-flex` : `d-none`}">
+      <svg width="1.5rem" height="1.5rem" class="me-2">
+        <use xlink:href="${iconSprite}#tabler-${args.icon}" />
+      </svg>
+    </div>
+    <div class="flex-grow-1 me-3">
+      <div class="d-flex">
+        <div class="flex-fill">
+          <p class="mb-0">File updated.</p>
+        </div>
+        <div class="flex-fill text-end">
+          <a href="#" class="text-white text-decoration-underline fw-normal ms-4">Action</a>
+        </div>
+      </div>
+    </div>
+    <div class="flex-shrink-1 mt-n1 mb-n1">
+      <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast" aria-label="Close">
+      </button>
+    </div>
+  </div>
+</div>
 
-ToastSuccess.parameters = {
+<div class="toast align-items-center text-white bg-${
+  args.class
+} border-0 fade show" role="alert" aria-live="assertive" aria-atomic="true" style="max-width: fit-content">
+  <div class="toast-body d-flex">
+    <div class="flex-shrink-1 ${args.show ? `d-flex` : `d-none`}">
+      <svg width="1.5rem" height="1.5rem" class="me-2">
+        <use xlink:href="${iconSprite}#tabler-${args.icon}" />
+      </svg>
+    </div>
+    <div class="flex-grow-1">
+      <div>
+        <p class="mb-0">
+          Multiple lines of text. Curabitur blandit temppus porttitor. Nullam id dolor id nibh ultricies vehicula.
+        </p>
+      </div>
+      <div class="text-end">
+        <a href="#" class="text-white text-decoration-underline fw-normal ms-4">Action</a>
+      </div>
+    </div>
+    <div class="flex-shrink-1 mt-n1 mb-n1">
+      <button type="button" class="btn-close btn-close-white" data-bs-dismiss="toast" aria-label="Close">
+      </button>
+    </div>
+  </div>
+</div>
+`
+
+export const Info = Toasts.bind()
+
+Info.args = {
+  icon: 'info-circle',
+  class: 'info',
+  show: true
+}
+
+Info.parameters = {
   notes: `
-  #Toast Success
+  #Info Toast
     
-  Toast Success alerts should be used to confirm that an important task or workflow has been completed.
+  Info Toasts should be used to confirm that an important task or workflow has been completed.
 
   ##Sample Code
   
   \`\`\`html
-  ${toast_success_html}
+  ${toast_html}
+  \`\`\`
+  `
+}
+
+export const Success = Toasts.bind()
+
+Success.args = {
+  icon: 'circle-check',
+  class: 'success',
+  show: true
+}
+
+Success.parameters = {
+  notes: `
+  #Success Toast
+    
+  Success Toasts should be used to confirm that an important task or workflow has been completed.
+
+  ##Sample Code
+  
+  \`\`\`html
+  ${toast_html}
+  \`\`\`
+  `
+}
+
+export const Error = Toasts.bind()
+
+Error.args = {
+  icon: 'alert-circle',
+  class: 'danger',
+  show: true
+}
+
+Error.parameters = {
+  notes: `
+  #Error Toast
+    
+  Error Toasts should be used to confirm that an important task or workflow has been completed.
+
+  ##Sample Code
+  
+  \`\`\`html
+  ${toast_html}
+  \`\`\`
+  `
+}
+
+export const Warning = Toasts.bind()
+
+Warning.args = {
+  icon: 'alert-triangle',
+  class: 'warning',
+  show: true
+}
+
+Warning.parameters = {
+  notes: `
+  #Warning Toast
+    
+  Warning Toasts should be used to confirm that an important task or workflow has been completed.
+
+  ##Sample Code
+  
+  \`\`\`html
+  ${toast_html}
+  \`\`\`
+  `
+}
+
+export const Neutral = Toasts.bind()
+
+Neutral.args = {
+  icon: '',
+  class: 'dark',
+  show: false
+}
+
+Neutral.parameters = {
+  notes: `
+  #Neutral Toast
+    
+  Neutral Toasts should be used to confirm that an important task or workflow has been completed.
+
+  ##Sample Code
+  
+  \`\`\`html
+  ${toast_html}
   \`\`\`
   `
 }
