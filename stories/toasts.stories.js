adb54f460d73d0587f976ba8f47d7ccc4111c4e9
Christian Boyle <mail@christianboyle.com>
2022-01-12 09:53:58 -0600

    add backstop reference for Toasts > Live

--- stories/toasts.stories.js
+++ stories/toasts.stories.js
@@ -12,7 +12,7 @@ const toast_live_html = `
   <button type="button" class="btn btn-primary" id="liveToastBtn">Show live toast</button>
 
   <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
-    <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div id="liveToast" class="toast w-sm-auto" role="alert" aria-live="assertive" aria-atomic="true">
       <div class="toast-header">
         <svg class="bd-placeholder-img rounded me-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#007aff"></rect></svg>
         <strong class="me-auto">Bootstrap</strong>
