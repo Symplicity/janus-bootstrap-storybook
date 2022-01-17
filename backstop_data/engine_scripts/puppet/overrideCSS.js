be78d17e51dd27ae609c1bb099a68aa5a3d6c3bd
Christian Boyle <mail@christianboyle.com>
2022-01-17 13:12:57 -0600

    migrate backstop from puppeteer to playwright, use latest docker image (6.0.4), fresh reference run

--- backstop_data/engine_scripts/puppet/overrideCSS.js
+++ backstop_data/engine_scripts/puppet/overrideCSS.js
@@ -1,12 +1,12 @@
-const BACKSTOP_TEST_CSS_OVERRIDE = `html {background-image: none;}`;
+const BACKSTOP_TEST_CSS_OVERRIDE = 'html {background-image: none;}';
 
-module.exports = function (page, scenario) {
+module.exports = async (page, scenario) => {
   // inject arbitrary css to override styles
-  page.evaluate(`window._styleData = '${BACKSTOP_TEST_CSS_OVERRIDE}'`);
-  page.evaluate(() => {
-    var style = document.createElement('style');
+  await page.evaluate(`window._styleData = '${BACKSTOP_TEST_CSS_OVERRIDE}'`);
+  await page.evaluate(() => {
+    const style = document.createElement('style');
     style.type = 'text/css';
-    var styleNode = document.createTextNode(window._styleData);
+    const styleNode = document.createTextNode(window._styleData);
     style.appendChild(styleNode);
     document.head.appendChild(style);
   });
