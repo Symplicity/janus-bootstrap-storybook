26a8b5b462d9c2f249e460c12633aee4339d1ae9
Christian Boyle <mail@christianboyle.com>
2022-03-10 12:24:30 -0600

    adjustments to wizard steps, part 5

--- stories/steps.stories.js
+++ stories/steps.stories.js
@@ -14,46 +14,48 @@ For additional documentation, see:
 `
 
 const steps_html = `
-<div class="d-flex">
-  <ol class="d-flex flex-nowrap list-unstyled align-items-center">
-    <li class="first done me-5 w-100">
-      <button class="btn btn-link">
-        <span class="step-icon bg-success rounded-circle text-white d-inline-block icon-xl-font-size">
-          <svg class="text-white icon-xl-font-size">
+<div class="float-start float-md-none d-flex justify-content-center position-relative steps four-steps">
+  <ol class="d-flex flex-nowrap list-unstyled align-items-center px-3 mb-0">
+    <li class="first done w-100">
+      <button class="btn btn-link text-decoration-none text-dark p-3">
+        <span class="step-icon bg-success rounded-circle text-white d-inline-block icon-xl-font-size mb-2">
+          <svg class="text-white icon-base-font-size">
             <use xlink:href="${iconSprite}#tabler-check" />
           </svg>
         </span>
         <span class="visually-hidden">Step: 1 Complete</span>
-        <span class="text-nowrap">Configure Collection</span>
+        <span class="text-nowrap d-block">Configure Collection</span>
       </button>
     </li>
-    <li class="done mx-5 w-100">
-      <button class="btn btn-link">
-        <span class="step-icon bg-success rounded-circle text-white d-inline-block icon-xl-font-size">
-          <svg class="text-white icon-xl-font-size">
+    <li class="done w-100">
+      <button class="btn btn-link text-decoration-none text-dark p-3">
+        <span class="step-icon bg-success rounded-circle text-white d-inline-block icon-xl-font-size mb-2">
+          <svg class="text-white icon-base-font-size">
             <use xlink:href="${iconSprite}#tabler-check" />
           </svg>
         </span>
         <span class="visually-hidden">Step: 2 Complete</span>
-        <span class="text-nowrap">Add Case Types</span>
+        <span class="text-nowrap d-block">Add Case Types</span>
       </button>
     </li>
-    <li class="current mx-5 w-100">
-      <button class="btn btn-link">
-        <span class="step-icon bg-primary rounded-circle text-white icon-xl-font-size d-inline-block">
-          3
+    <li class="current w-100">
+      <button class="btn btn-link text-decoration-none text-dark p-3">
+        <span class="step-number bg-primary rounded-circle text-white icon-xl-font-size d-inline-block mb-2">
+          <span class="fs-6 d-flex align-items-center justify-content-center">3</span>
         </span>
         <span class="visually-hidden">Step: 3 Current</span>
-        <span class="text-nowrap">Review</span>
+        <span class="text-nowrap d-block">
+          <strong>Review</strong>
+        </span>
       </button>
     </li>
-    <li class="disabled last ms-5 w-100">
-      <button class="btn btn-link">
-        <span class="step-icon bg-light rounded-circle text-dark icon-xl-font-size d-inline-block">
-          4
+    <li class="disabled last w-100">
+      <button class="btn btn-link text-decoration-none text-dark p-3">
+        <span class="step-number rounded-circle icon-xl-font-size d-inline-block mb-2">
+          <span class="fs-6 d-flex align-items-center justify-content-center">4</span>
         </span>
         <span class="visually-hidden">Step: 4</span>
-        <span class="text-nowrap">What's Next?</span>
+        <span class="text-nowrap d-block">What's Next?</span>
       </button>
     </li>
   </ol>
@@ -62,7 +64,7 @@ const steps_html = `
 
 export const Steps = () => {
   const container = $(
-    '<div class="text-center" style="margin-top:100px"></div>'
+    '<div class="text-center" style="margin:100px 0 0;"></div>'
   ).append(steps_html)
 
   return container[0]
