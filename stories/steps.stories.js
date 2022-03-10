82bad606db0fdf3d233cdca6130f4fcbc0083b3e
Christian Boyle <mail@christianboyle.com>
2022-03-10 13:08:11 -0600

    adjustments to wizard steps, part 6

--- stories/steps.stories.js
+++ stories/steps.stories.js
@@ -18,44 +18,44 @@ const steps_html = `
   <ol class="d-flex flex-nowrap list-unstyled align-items-center px-3 mb-0">
     <li class="first done w-100">
       <button class="btn btn-link text-decoration-none text-dark p-3">
-        <span class="step-icon bg-success rounded-circle text-white d-inline-block icon-xl-font-size mb-2">
+        <span class="step-icon bg-success rounded-circle text-white d-inline-block icon-xl-font-size mb-6">
           <svg class="text-white icon-base-font-size">
             <use xlink:href="${iconSprite}#tabler-check" />
           </svg>
         </span>
         <span class="visually-hidden">Step: 1 Complete</span>
-        <span class="text-nowrap d-block">Configure Collection</span>
+        <span class="text-nowrap d-block lh-1">Configure Collection</span>
       </button>
     </li>
     <li class="done w-100">
       <button class="btn btn-link text-decoration-none text-dark p-3">
-        <span class="step-icon bg-success rounded-circle text-white d-inline-block icon-xl-font-size mb-2">
+        <span class="step-icon bg-success rounded-circle text-white d-inline-block icon-xl-font-size mb-6">
           <svg class="text-white icon-base-font-size">
             <use xlink:href="${iconSprite}#tabler-check" />
           </svg>
         </span>
         <span class="visually-hidden">Step: 2 Complete</span>
-        <span class="text-nowrap d-block">Add Case Types</span>
+        <span class="text-nowrap d-block lh-1">Add Case Types</span>
       </button>
     </li>
     <li class="current w-100">
       <button class="btn btn-link text-decoration-none text-dark p-3">
-        <span class="step-number bg-primary rounded-circle text-white icon-xl-font-size d-inline-block mb-2">
+        <span class="step-number bg-primary rounded-circle text-white icon-xl-font-size d-inline-block mb-6">
           <span class="fs-6 d-flex align-items-center justify-content-center">3</span>
         </span>
         <span class="visually-hidden">Step: 3 Current</span>
-        <span class="text-nowrap d-block">
+        <span class="text-nowrap d-block lh-1">
           <strong>Review</strong>
         </span>
       </button>
     </li>
     <li class="disabled last w-100">
       <button class="btn btn-link text-decoration-none text-dark p-3">
-        <span class="step-number rounded-circle icon-xl-font-size d-inline-block mb-2">
+        <span class="step-number rounded-circle icon-xl-font-size d-inline-block mb-6">
           <span class="fs-6 d-flex align-items-center justify-content-center">4</span>
         </span>
         <span class="visually-hidden">Step: 4</span>
-        <span class="text-nowrap d-block">What's Next?</span>
+        <span class="text-nowrap d-block lh-1">What's Next?</span>
       </button>
     </li>
   </ol>
