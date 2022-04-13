328e553500e725abd593b33410147316799f0074
Christian Boyle <mail@christianboyle.com>
2022-04-13 08:21:34 -0500

    action-bar refinement, spacing and borders

--- stories/action-bar.stories.js
+++ stories/action-bar.stories.js
@@ -14,38 +14,38 @@ For additional documentation, see:
 `
 
 const action_bar_html = `
-<div class="action-bar-container position-absolute align-items-center end-0 bg-light justify-content-md-end" tabindex="0">
-  <div class="action-bar fixed-bottom d-flex flex-row flex-md-column align-items-center align-self-end align-self-md-center justify-content-center bg-light">
-    <ul class="nav nav-pills nav-flush flex-row flex-md-column mb-0 text-center d-flex-column flex-nowrap">
-      <li class="nav-item">
+<div class="action-bar-container position-absolute align-items-center end-0 justify-content-md-end" tabindex="0">
+  <div class="action-bar fixed-bottom d-flex flex-row flex-md-column align-items-center align-self-end align-self-md-center justify-content-center">
+    <ul class="nav nav-pills nav-flush flex-row flex-md-column mb-0 text-center d-flex-column flex-nowrap w-100 justify-content-between justify-content-md-center">
+      <li class="nav-item pb-md-2">
         <a href="#" class="nav-link active p-3" aria-current="page" title="" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-original-title="Home">
           <svg class="icon-lg-font-size">
             <use xlink:href="${iconSprite}#tabler-check" role="image" aria-label="Home" />
           </svg>
         </a>
       </li>
-      <li>
+      <li class="nav-item py-md-2">
         <a href="#" class="nav-link p-3" title="" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-original-title="Dashboard">
           <svg class="icon-lg-font-size">
             <use xlink:href="${iconSprite}#tabler-check" role="image" aria-label="Home" />
           </svg>
         </a>
       </li>
-      <li>
+      <li class="nav-item py-md-2">
         <a href="#" class="nav-link p-3" title="" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-original-title="Orders">
           <svg class="icon-lg-font-size">
             <use xlink:href="${iconSprite}#tabler-check" role="image" aria-label="Home" />
           </svg>
         </a>
       </li>
-      <li>
+      <li class="nav-item px-md-0 py-md-2">
         <a href="#" class="nav-link p-3" title="" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-original-title="Products">
           <svg class="icon-lg-font-size">
             <use xlink:href="${iconSprite}#tabler-check" role="image" aria-label="Home" />
           </svg>
         </a>
       </li>
-      <li>
+      <li class="nav-item px-md-0 pt-md-2">
         <a href="#" class="nav-link p-3" title="" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-original-title="Customers">
           <svg class="icon-lg-font-size">
             <use xlink:href="${iconSprite}#tabler-check" role="image" aria-label="Home" />
@@ -59,7 +59,7 @@ const action_bar_html = `
 
 export const ActionBar = () => {
   const container = $(
-    '<div style="position: unset !important; height: 100%; margin: 0; box-shadow: 0 0 5px 2px red inset;"></div>'
+    '<div style="position: unset !important; height: 100%; margin: 0;"></div>'
   ).append(action_bar_html)
 
   if (!('ontouchstart' in window)) {
