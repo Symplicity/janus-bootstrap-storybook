5e93ce12da02485205708502346478e5ae44c9ec
Christian Boyle <mail@christianboyle.com>
2022-04-29 10:48:05 -0500

    add missing backstop reference for pagination story

--- stories/pagination.stories.js
+++ stories/pagination.stories.js
@@ -1,12 +1,12 @@
-import iconSprite from '../node_modules/@symplicity/tabler-icons/symplicity/tabler-sprite-nostroke.svg';
+import iconSprite from '../node_modules/@symplicity/tabler-icons/symplicity/tabler-sprite-nostroke.svg'
 
 export default {
-  title: 'Components/Pagination',
-};
+  title: 'Components/Pagination'
+}
 
 const paginationHTML = `
 <div>
-  <div class="d-flex align-items-center">
+  <div class="d-flex align-items-center justify-content-center">
     <div class="d-flex">
       <button type="button" class="btn btn-action btn-action-icon-only rounded-circle border-light">
         <svg class="icon-lg-font-size mt-n1">
@@ -32,7 +32,7 @@ const paginationHTML = `
     </div>
   </div>
 </div>
-`;
+`
 
 const moreDocumentation = `
 #Additional Documentation
@@ -41,11 +41,12 @@ For additional documentation, see:
 - Bootstrap Documentation
 - Design System 
 
-`;
+`
 
-export const Pagination = () => `<div style="margin-top:100px">${paginationHTML}</div>`;
+export const Pagination = () =>
+  `<div style="margin-top:100px;">${paginationHTML}</div>`
 
-Pagination.storyName = 'Pagination';
+Pagination.storyName = 'Pagination'
 
 Pagination.parameters = {
   notes: `
@@ -63,5 +64,5 @@ Pagination.parameters = {
     
     ${moreDocumentation}
     
-    `,
-};
+    `
+}
