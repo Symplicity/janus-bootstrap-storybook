61a42b8ace2e7bd656ecd5d2d52e9c4fd83a93e7
Christian Boyle <mail@christianboyle.com>
2022-06-23 11:00:16 -0500

    update collection list tbody elements to have borders, per jlung

--- stories/example-list-collection.stories.js
+++ stories/example-list-collection.stories.js
@@ -55,7 +55,7 @@ const listCollectionHTML = `
         </tr>
       </tbody>
 
-      <tbody role="rowgroup" class="expandable-row-child border-0 show">
+      <tbody role="rowgroup" class="expandable-row-child show">
         <tr id="content-1" role="row">
           <td role="cell" headers="column-1" class="px-3 text-nowrap">
             <button class="btn btn-link fw-normal mt-n1 d-inline-flex fs-5 p-0 lh-lg">Academic Misconduct</button>
@@ -213,6 +213,75 @@ const listCollectionHTML = `
             </div>
           </td>
           <td role="cell" headers="column-4" class="px-3">
+            <div class="d-inline-flex">
+              <button class="btn btn-action d-flex py-1 px-2" type="button" aria-label="Copy Link to Public Form">
+                <svg class="icon-base-font-size">
+                  <use xlink:href="${iconSprite}#tabler-copy" />
+                </svg>
+              </button>
+              <button class="btn btn-action d-flex py-1 px-2" type="button" aria-label="View Case Type">
+                <svg class="icon-base-font-size">
+                  <use xlink:href="${iconSprite}#tabler-link" />
+                </svg>
+              </button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+
+      <tbody role="rowgroup" class="expandable-row-parent">
+        <tr role="row" class="bg-light shadow-none">
+          <th id="row-1" class="lh-1 p-3" colspan="4" scope="colgroup">
+            <div class="d-flex align-items-center">
+              <div class="flex-fill fw-normal">
+                <div>
+                  <span class="text-muted me-1">Collection:</span> <button role="link" class="btn btn-link fw-normal mt-n1 d-inline-flex fs-5 p-0 lh-lg minw-0" aria-label="View Collection Overview">Incidents</button>
+                  <button type="button" class="btn btn-action p-0 d-inline-flex text-muted" aria-label="Copy Link to Public Form">
+                    <svg class="icon-base-font-size">
+                      <use xlink:href="${iconSprite}#tabler-link" />
+                    </svg>
+                  </button>
+                </div>
+                <div class="mb-1">
+                  <span class="text-muted me-1">Status:</span> <span class="text-black">Active</span>
+                </div>
+              </div>
+              <div class="d-flex flex-fill align-items-center justify-content-end">
+                <div class="d-inline-flex">
+                  <button type="button" class="btn btn-action rounded-2 active py-0" aria-label="Add Case to TitleIX Reports">
+                    <span class="fs-5 lh-lg">Add Case Type</span> 
+                  </button>
+                </div>
+                <div class="d-inline-flex">
+                  <button class="btn btn-action d-flex py-1 px-2 ms-3 row-toggle" type="button" data-bs-toggle="collapse" data-bs-target=".collapse" aria-expanded="true" aria-controls="content-1 content-2 content-3 content-4" aria-label="Expand Collection to View Associated Case Types">
+                    <svg class="icon-lg-font-size">
+                      <use xlink:href="${iconSprite}#tabler-chevron-down" />
+                      <span class="visually-hidden">Expand Row</span>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </th>
+        </tr>
+      </tbody>
+
+      <tbody role="rowgroup" class="expandable-row-child show">
+        <tr id="content-7" role="row">
+          <td role="cell" headers="column-1" class="px-3 text-nowrap">
+            <button class="btn btn-link fw-normal mt-n1 d-inline-flex fs-5 p-0 lh-lg">Academic Misconduct</button>
+            <span class="badge bg-success rounded-pill text-uppercase lh-1 fs-7">Published</span>
+          </td>
+          <td role="cell" headers="column-2" class="px-3 text-nowrap">
+            <span>Oct 1, 2021</span>
+          </td>
+          <td role="cell" headers="column-3" class="px-3 text-nowrap">
+            <div class="form-check form-switch m-0 p-0 d-flex align-items-center">
+              <input class="form-check-input m-2 ms-0" type="checkbox" role="switch" id="flexSwitchCheckChecked1" checked>
+              <label class="form-check-label" for="flexSwitchCheckChecked1" aria-label="Case Type Active">Active</label>
+            </div>
+          </td>
+          <td role="cell" headers="column-4" class="px-3 text-nowrap">
             <div class="d-inline-flex">
               <button class="btn btn-action d-flex py-1 px-2" type="button" aria-label="Copy Link to Public Form">
                 <svg class="icon-base-font-size">
