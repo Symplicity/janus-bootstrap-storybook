17723918587e38d67b886f604fe95e599a681657
Christian Boyle <mail@christianboyle.com>
2022-07-27 16:03:04 -0500

    refinement to action-bar hover and focus styles

--- stories/action-bar.stories.js
+++ stories/action-bar.stories.js
@@ -16,36 +16,36 @@ For additional documentation, see:
 const action_bar_html = `
 <div class="action-bar-container position-absolute align-items-center end-0 justify-content-md-end" tabindex="0">
   <div class="action-bar fixed-bottom d-flex flex-row flex-md-column align-items-center align-self-end align-self-md-center justify-content-center">
-    <ul class="nav nav-pills nav-flush flex-row flex-md-column mb-0 text-center d-flex-column flex-nowrap w-100 justify-content-between justify-content-md-center">
-      <li class="nav-item pb-md-2">
+    <ul class="nav nav-pills nav-flush flex-row flex-md-column mb-0 text-center d-flex-column flex-nowrap  justify-content-between justify-content-md-center">
+      <li class="nav-item px-md-0 py-md-2 mx-md-0 mx-2">
         <a href="#" class="nav-link active p-3" aria-current="page" aria-label="Inbox" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-original-title="Inbox">
           <svg class="icon-lg-font-size">
             <use xlink:href="${iconSprite}#tabler-inbox" />
           </svg>
         </a>
       </li>
-      <li class="nav-item py-md-2">
+      <li class="nav-item px-md-0 py-md-2 mx-md-0 mx-2">
         <a href="#" class="nav-link p-3" aria-label="Todo" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-original-title="Todo">
           <svg class="icon-lg-font-size">
             <use xlink:href="${iconSprite}#tabler-list-check" />
           </svg>
         </a>
       </li>
-      <li class="nav-item py-md-2">
+      <li class="nav-item px-md-0 py-md-2 mx-md-0 mx-2">
         <a href="#" class="nav-link p-3" aria-label="Notes" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-original-title="Notes">
           <svg class="icon-lg-font-size">
             <use xlink:href="${iconSprite}#tabler-note"  />
           </svg>
         </a>
       </li>
-      <li class="nav-item px-md-0 py-md-2">
+      <li class="nav-item px-md-0 py-md-2 mx-md-0 mx-2">
         <a href="#" class="nav-link p-3" aria-label="Calendar" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-original-title="Calendar">
           <svg class="icon-lg-font-size">
             <use xlink:href="${iconSprite}#tabler-calendar" />
           </svg>
         </a>
       </li>
-      <li class="nav-item px-md-0 pt-md-2">
+      <li class="nav-item px-md-0 pt-md-2 mx-md-0 mx-2">
         <a href="#" class="nav-link p-3" aria-label="Other" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-original-title="Other">
           <svg class="icon-lg-font-size">
             <use xlink:href="${iconSprite}#tabler-dots" />
